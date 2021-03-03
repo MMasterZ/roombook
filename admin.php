6b87456631bcc615390981cf634a8b9db45590c8
Tanin <ber_zebub@hotmail.com>
2021-03-04 00:48:49 +0700

    update admin

--- admin.php
+++ admin.php
@@ -6,13 +6,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
 </head>
 
 <body class='bg'>
     <!-- LAYOUT -->
-    <div class='row '>
-        <div class='col-md-6 brx'>
-            1234</div>
+    <div class="">
+        <div class="row align-items-start">
+            <div class="" style='width:320px;background-color:black;height:95vh;color:white'>
+                <div align="center">
+                    Admin Menu
+                </div>
+                <br>
+                <div style='padding:10px'>
+                    <div>
+
+                    </div>
+                </div>
+                <div style='padding:10px'>
+                    <button type="button" class="btn btn-outline-secondary" style='width:100%'>ข้อมูลลูกค้า</button>
+                </div>
+                <div style='padding:10px'>
+                    <button type="button" class="btn btn-outline-secondary"
+                        style='width:100%'>ตรวจสอบสถานะห้องพัก</button>
+                </div>
+                <div style='padding:10px'>
+                    <button type="button" class="btn btn-outline-secondary" style='width:100%'>จัดการห้องพัก</button>
+                </div>
+
+            </div>
+            <div class="col">
+                One of three columns
+            </div>
+            <div class="col">
+                One of three columns
+            </div>
+        </div>
     </div>
 </body>
 
