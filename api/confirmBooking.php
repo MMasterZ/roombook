062166418f38bcf11756721b70725729117b5a8a
Tanin <ber_zebub@hotmail.com>
2021-03-09 23:28:54 +0700

    Update

--- api/confirmBooking.php
+++ api/confirmBooking.php
@@ -1,10 +1,27 @@
 <?php 
+error_reporting(-1);
+ini_set('display_errors', 'On');
 require("../Connections/conn.php");
 session_start();
+
+function generateRandomString($length = 10) {
+    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
+
     $check_in = $_POST['checkIn'];
     $check_out = $_POST['checkOut'];
     $user_id = $_SESSION['user_id'];
-    $sql_update = "insert into booking_log (user_id,check_in_date,check_out_date) values('$user_id','$check_in','$check_out')";
+    $room_type = $_POST['roomType'];
+    $name = $_POST['name'];
+    $surname = $_POST['surname'];
+    $tel = $_POST['tel'];
+    $email = $_POST['email'];
+    $fname = $_FILES['file']['tmp_name'];
+    $real_name = $user_id . generateRandomString() . ".jpg";
+    $uploads_dir = "../payment_document";
+    move_uploaded_file($fname, "$uploads_dir/$real_name");
+
+    $sql_update = "insert into booking_log (user_id,check_in_date,check_out_date,room_type,name,surname,tel,email,payment_file) values('$user_id','$check_in','$check_out','$room_type','$name','$surname','$tel','$email','$real_name')";
     $result = $mysqli -> query($sql_update);
-    echo "success";
+    echo $sql_update;
 ?>
