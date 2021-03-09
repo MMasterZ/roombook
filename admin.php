bb7c99d34cc9f781c155e50bb160ec4839125b19
MMasterZ <soulhunz@hotmail.com>
2021-03-09 13:54:47 +0700

    update roombook

--- admin.php
+++ admin.php
@@ -1,82 +1,84 @@
-<!DOCTYPE html>
+<?php 
+include_once("Connections/conn.php");
+?>
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
-    </script>
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
-    <script type="text/javascript" src="js/jquery.min.js"></script>
-    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
-    <title>บ้านดินถิ่นวาปี</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous">
+  </script>
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+  <script type="text/javascript" src="js/jquery.min.js"></script>
+  <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+  <title>บ้านดินถิ่นวาปี</title>
 </head>
 <script>
 $(document).ready(function() {
 
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
 
 
 
-    const getParams = urlParams.get("menu")
+  const getParams = urlParams.get("menu")
 
 
 
 
-    if (getParams == 1) {
-        $("#user_info").addClass("active")
-    } else if (getParams == 2) {
-        $("#room_status").addClass("active")
-    } else if (getParams == 3) {
-        $("#manage_room").addClass("active")
+  if (getParams == 1) {
+    $("#user_info").addClass("active")
+  } else if (getParams == 2) {
+    $("#room_status").addClass("active")
+  } else if (getParams == 3) {
+    $("#manage_room").addClass("active")
 
-    }
+  }
 
-    $("#user_info").click(function() {
-        window.location.href = "/roombook/admin.php?menu=1"
-    })
-    $("#room_status").click(function() {
-        window.location.href = "/roombook/admin.php?menu=2"
-    })
-    $("#manage_room").click(function() {
-        window.location.href = "/roombook/admin.php?menu=3"
-    })
+  $("#user_info").click(function() {
+    window.location.href = "/roombook/admin.php?menu=1"
+  })
+  $("#room_status").click(function() {
+    window.location.href = "/roombook/admin.php?menu=2"
+  })
+  $("#manage_room").click(function() {
+    window.location.href = "/roombook/admin.php?menu=3"
+  })
 
 })
 </script>
 
 <body class='bg'>
-    <!-- LAYOUT -->
-    <div class="">
-        <div class="row align-items-start">
-            <div class="" style='width:320px;background-color:black;height:99vh;color:white'>
-                <div align="center">
-                    Admin Menu xx
-                </div>
-                <br>
-                <div style='padding:10px'>
-                </div>
-                <div style='padding:10px'>
-                    <button id="user_info" type="button" class="btn btn-outline-secondary"
-                        style='width:100%'>ข้อมูลลูกค้า</button>
-                </div>
-                <div style='padding:10px'>
-                    <button id="room_status" type="button" class="btn btn-outline-secondary"
-                        style='width:100%'>ตรวจสอบการจองห้องพัก</button>
-                </div>
-                <div style='padding:10px'>
-                    <button id="manage_room" type="button" class="btn btn-outline-secondary"
-                        style='width:100%'>จัดการห้องพัก</button>
-                </div>
+  <!-- LAYOUT -->
+  <div class="">
+    <div class="row align-items-start">
+      <div class="" style='width:320px;background-color:black;height:99vh;color:white'>
+        <div align="center">
+          Admin Menu xx
+        </div>
+        <br>
+        <div style='padding:10px'>
+        </div>
+        <div style='padding:10px'>
+          <button id="user_info" type="button" class="btn btn-outline-secondary"
+            style='width:100%'>ข้อมูลลูกค้า</button>
+        </div>
+        <div style='padding:10px'>
+          <button id="room_status" type="button" class="btn btn-outline-secondary"
+            style='width:100%'>ตรวจสอบการจองห้องพัก</button>
+        </div>
+        <div style='padding:10px'>
+          <button id="manage_room" type="button" class="btn btn-outline-secondary"
+            style='width:100%'>จัดการห้องพัก</button>
+        </div>
 
-            </div>
-            <div class="col " align="center">
-                <?php
+      </div>
+      <div class="col " align="center">
+        <?php
 if($_GET['menu'] == 1)
 {
     include("admin/user_info.php");
@@ -86,20 +88,20 @@ if($_GET['menu'] == 1)
     include("admin/manage_room.php");
 }
  ?>
-            </div>
+      </div>
 
-        </div>
     </div>
+  </div>
 </body>
 
 <style>
 .bg {
-    background-image: url('img/1.jpg');
-    background-size: cover
+  background-image: url('img/1.jpg');
+  background-size: cover
 }
 
 .brx {
-    border: 1px solid red
+  border: 1px solid red
 }
 </style>
 
--- admin/room_status.php
+++ admin/room_status.php
@@ -1,28 +1,27 @@
-<?php require("Connections/conn.php");  ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <body>
 
-    <div class="container p-5">
-        <div class='card p-3' style='min-height:85vh;'>
+  <div class="container p-5">
+    <div class='card p-3' style='min-height:85vh;'>
 
-            <div class='h3 p-2'>
-                สถานะการจองห้องพัก
-            </div>
-            <table style="width:100%;border-collapse:collapse">
-                <tr>
-                    <th>ผู้จอง</th>
-                    <th>Check In Date</th>
-                    <th>Check Out Date</th>
-                </tr>
-                <?php 
+      <div class='h3 p-2'>
+        สถานะการจองห้องพัก
+      </div>
+      <table style="width:100%;border-collapse:collapse">
+        <tr>
+          <th>ผู้จอง</th>
+          <th>Check In Date</th>
+          <th>Check Out Date</th>
+        </tr>
+        <?php 
                 $sql = "select * from booking_log";
                 $result = $mysqli -> query("$sql");
                 foreach($result as $item){ ?>
-                <tr>
-                    <td>
-                        <?php  
+        <tr>
+          <td>
+            <?php  
                         $user_id = $item['user_id'];
                         $sql_get_name = "select * from user where id = $user_id";
                         $result_name = $mysqli -> query($sql_get_name);
@@ -30,36 +29,36 @@
                             echo $item2['name'];
                         }
                      ?>
-                    </td>
-                    <td>
-                        <?php echo $item['check_in_date'];  ?>
-                    </td>
-                    <td>
-                        <?php echo $item['check_out_date'];  ?>
-                    </td>
-                </tr>
-                <?php } ?>
-            </table>
-        </div>
+          </td>
+          <td>
+            <?php echo $item['check_in_date'];  ?>
+          </td>
+          <td>
+            <?php echo $item['check_out_date'];  ?>
+          </td>
+        </tr>
+        <?php } ?>
+      </table>
     </div>
+  </div>
 
 </body>
 
 <style>
 .brx {
-    border: 1px solid red
+  border: 1px solid red
 }
 
 th,
 td {
-    text-align: center
+  text-align: center
 }
 
 table,
 tr,
 th,
 td {
-    border: 1px solid grey
+  border: 1px solid grey
 }
 </style>
 
