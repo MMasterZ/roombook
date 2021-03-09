b986c523bf1605d6aa98725f6d5f3d53c86463ba
Tanin <ber_zebub@hotmail.com>
2021-03-10 01:28:22 +0700

    update room status

--- admin/room_status.php
+++ admin/room_status.php
@@ -1,64 +1,104 @@
 <!DOCTYPE html>
 <html lang="en">
 
+<script>
+$(document).ready(function(){
+  $(".checkBox").change(function(){
+    if($(this).is(":checked")){
+      console.log("checked");
+      $.post("api/updateRoomStatus.php",
+      {
+        id : $(this).attr('item'),
+        status : '1'
+      }).done(function(res){
+      })
+    }else{
+      $.post("api/updateRoomStatus.php",
+      {
+        id : $(this).attr('item'),
+        status : '0'
+      }).done(function(res){
+      })
+    }
+  })
+})
+</script>
 <body>
 
-  <div class="container p-5">
-    <div class='card p-3' style='min-height:85vh;'>
+    <div class="container p-5">
+        <div class='card p-3' style='min-height:85vh;'>
 
-      <div class='h3 p-2'>
-        สถานะการจองห้องพัก
-      </div>
-      <table style="width:100%;border-collapse:collapse">
-        <tr>
-          <th>ผู้จอง</th>
-          <th>Check In Date</th>
-          <th>Check Out Date</th>
-        </tr>
-        <?php 
+            <div class='h3 p-2'>
+                สถานะการจองห้องพัก
+            </div>
+            <table style="width:100%;border-collapse:collapse">
+                <tr>
+                    <th>ผู้จอง</th>
+                    <th>Check In Date</th>
+                    <th>Check Out Date</th>
+                    <th>ประเภทห้อง</th>
+                    <th>เบอร์โทร</th>
+                    <th>อีเมล</th>
+                    <th>เอกสารการชำระเงิน</th>
+                    <th>สถานะ</th>
+                </tr>
+                <?php 
                 $sql = "select * from booking_log";
                 $result = $mysqli -> query("$sql");
                 foreach($result as $item){ ?>
-        <tr>
-          <td>
-            <?php  
+                <tr>
+                    <td>
+                        <?php  
                         $user_id = $item['user_id'];
-                        $sql_get_name = "select * from user where id = $user_id";
-                        $result_name = $mysqli -> query($sql_get_name);
-                        foreach ($result_name as $item2) {
-                            echo $item2['name'];
-                        }
+                        echo $item['name'] . " " . $item['surname'];
                      ?>
-          </td>
-          <td>
-            <?php echo $item['check_in_date'];  ?>
-          </td>
-          <td>
-            <?php echo $item['check_out_date'];  ?>
-          </td>
-        </tr>
-        <?php } ?>
-      </table>
+                    </td>
+                    <td>
+                        <?php echo $item['check_in_date'];  ?>
+                    </td>
+                    <td>
+                        <?php echo $item['check_out_date'];  ?>
+                    </td>
+                    <td>
+                        <?php echo $item['room_type'];  ?>
+                    </td>
+                    <td>
+                        <?php echo $item['tel'];  ?>
+                    </td>
+                    <td>
+                        <?php echo $item['email'];  ?>
+                    </td>
+                    <td>
+                        <a target="_blank" href="./payment_document/<?php echo $item['payment_file']; ?>">
+                            ดูเอกสารการชำระเงิน
+                        </a>
+                    </td>
+                    <td>
+                      <input class='checkBox' item="<?php echo $item['id']; ?>" type="checkbox" <?php if($item['status'] == '1'){ echo "checked"; } ?>>
+                    </td>
+                </tr>
+                <?php } ?>
+            </table>
+        </div>
     </div>
-  </div>
 
 </body>
 
 <style>
 .brx {
-  border: 1px solid red
+    border: 1px solid red
 }
 
 th,
 td {
-  text-align: center
+    text-align: center
 }
 
 table,
 tr,
 th,
 td {
-  border: 1px solid grey
+    border: 1px solid grey
 }
 </style>
 
